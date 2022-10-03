a1e79afe8c7f254583f684194d934c253153489a
payal9876 <payalyadav9456444@gmail.com>
2022-10-03 12:22:51 +0530

    fifth commit

--- app.py
+++ app.py
@@ -17,16 +17,17 @@ def home():
 @app.route('/',methods=['POST'])
 def predict():
 
-    FUNDING_ROUNDS=int(request.form["funding_rounds"])
-    TOTAL_FUNDING_USD=int(request.form['TOTAL_FUNDING_USD'])
-    MILESTONES=int(request.form['MILESTONES'])
-    RELATIONSHIPS=int(request.form['relationships'])
-    latitude=float(request.form['latitude'])
-    longitude=float(request.form['longitude'])
+    funding_rounds=int(request.form["funding_rounds"])
+    funding_total_usd=int(request.form['TOTAL_FUNDING_USD'])
+    milestones=int(request.form['MILESTONES'])
+    relationships=int(request.form['relationships'])
+    lat=float(request.form['latitude'])
+    lng=float(request.form['longitude'])
     active_days=int(request.form['active_days'])
     
+    data=['funding_rounds','funding_total_usd','milestones','relationships','lat','lng','active_days']
 
-    data=['FUNDING_ROUNDS','TOTAL_FUNDING_USD','MILESTONES','RELATIONSHIPS','latitude','longitude','active_days']    
+    
     features_value=[np.array(data)]
 
     feature_name=['funding_rounds','funding_total_usd','milestones','relationships','lat','lng','active_days']
